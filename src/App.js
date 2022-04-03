5d31d62a2579e2a3fac45b79951d25382b0956fc
Anirudh Sharma <76739126+anirudh357@users.noreply.github.com>
2022-04-04 02:22:32 +0530

    Add files via upload
    All Files uploaded.

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect } from 'react';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import logo from './images/logo.png';
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
+
+
+const alanKey = '08593dc4c0d17266ae889c8004cee7332e956eca572e1d8b807a3e2338fdd0dc/stage'
+
+const App = () => {
+    const [activeArticle, setActiveArticle] = useState(0);
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const classes = useStyles();
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles, number }) => {
+                if (command === 'newHeadlines') {
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                  } else if (command === 'instructions') {
+                    setIsOpen(true);
+                  } else if (command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                  } else if (command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+          
+                    if (parsedNumber > articles.length) {
+                      alanBtn().playText('Please try that again...');
+                    } else if (article) {
+                      window.open(article.url, '_blank');
+                      alanBtn().playText('Opening...');
+                    } else {
+                      alanBtn().playText('Please try that again...');
+                    }
+                  }
+            },
+        });
+    }, []);
+
+    return (
+        <div>
+        <div className={classes.logoContainer} >
+          
+          <img src="https://46ba123xc93a357lc11tqhds-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/alan.jpg" className={classes.alanLogo} alt="logo" />
+        </div>
+        <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+        
+        <div className='footer__copyright' >
+      &copy; 2022 Made with ❤️ by{' '}
+      <a
+        className='footer__copyright--link'
+        href='https://github.com/anirudh357'
+      >
+        {' '}
+        Anirudh Sharma
+      </a>
+    </div>
+      </div>
+    );  
+}
+
+export default App;
